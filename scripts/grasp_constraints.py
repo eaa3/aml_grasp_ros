2e9bad70e689f4f2ae5eaaaf13695140ade85939
Ermano Arruda <ermano.arruda@gmail.com>
2019-09-24 13:00:03 +0100

    re-adding constraints

--- scripts/grasp_constraints.py
+++ scripts/grasp_constraints.py
@@ -2,7 +2,7 @@ import numpy as np
 from aml_math import *
 import robot_kinematics as jk
 
-# kin = jk.robot_kinematics()
+kin = jk.robot_kinematics()
 
 def axis_alignment_constraint(manipulator, solution, **kwargs):
 
@@ -41,10 +41,14 @@ def hard_kinematic_constraint(manipulator, solution, **kwargs):
     #solution.skip = not kin.kdl_inverse_kinematics_jl(solution.base_pose[0], solution.base_pose[1], None)
     
     grasp_ok = True
-    # for pose in solution.base_trajectory:
-    #     grasp_ok = grasp_ok and kin.trac_ik_inverse_kinematics(pose.p, pose.q, None)
-    
-    solution.skip = not kin.trac_ik_inverse_kinematics(solution.base_pose[0], solution.base_pose[1], None)
+    for pose in solution.base_trajectory:
+        grasp_ok = grasp_ok and kin.trac_ik_inverse_kinematics(pose.p, pose.q, None)
+        if not grasp_ok:
+            break
+            
+    solution.skip = not grasp_ok
+
+    #solution.skip = not kin.trac_ik_inverse_kinematics(solution.base_pose[0], solution.base_pose[1], None)
     #solution.skip = solution.skip or solution.base_pose[0][2] < -0.28
     # solution.skip = solution.skip or not np.all([e for e,_ in solution.contact_mask.items()])
     #print "Kinematics constraint: ",solution.skip
